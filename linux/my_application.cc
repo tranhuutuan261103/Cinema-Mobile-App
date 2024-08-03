ae3767b1b799e225641d5deb64c4d539bad9d549
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-08-04 00:28:07 +0700

    feat: [Cinema Mobile App] Change icon and name of app

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "cinema_mobile_app");
+    gtk_header_bar_set_title(header_bar, "Tune Cinema");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "cinema_mobile_app");
+    gtk_window_set_title(window, "Tune Cinema");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
