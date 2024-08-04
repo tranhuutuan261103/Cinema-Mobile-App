17e95d307152dbc17eea9225331cd5cd505f93e2
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-08-05 00:32:35 +0700

    feat: [Cinema Mobile App] Design detail movie screen

--- lib/widgets/movie_poster.dart
+++ lib/widgets/movie_poster.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/movie.dart';
+import '../screens/movie_detail_screen.dart'; // Import your detail screen
 
 class MoviePoster extends StatelessWidget {
   const MoviePoster({super.key, required this.movie});
@@ -8,61 +9,72 @@ class MoviePoster extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start, // Align the column content to the start
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: Image.network(
-            movie.imageUrl,
-            fit: BoxFit.cover,
-            height: 240,
-            width: 180,
+    return InkWell(
+      onTap: () {
+        // Navigate to the detail screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MovieDetailScreen(movie: movie),
           ),
-        ),
-        const SizedBox(height: 8),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start, // Align children to the start
-          children: [
-            const Icon(
-              Icons.star,
-              color: Colors.amber,
-              size: 16,
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Align the column content to the start
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.network(
+              movie.imageUrl,
+              fit: BoxFit.cover,
+              height: 240,
+              width: 180,
             ),
-            const SizedBox(width: 4), // Spacing between icon and text
-            Text(
-              movie.rating.toString(),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start, // Align children to the start
+            children: [
+              const Icon(
+                Icons.star,
+                color: Colors.amber,
+                size: 16,
+              ),
+              const SizedBox(width: 4), // Spacing between icon and text
+              Text(
+                movie.rating.toString(),
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 180,
+            child: Text(
+              movie.title,
               style: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
               overflow: TextOverflow.ellipsis,
             ),
-          ],
-        ),
-        SizedBox(
-          width: 180,
-          child: Text(
-            movie.title,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-            overflow: TextOverflow.ellipsis,
           ),
-        ),
-        SizedBox(
-          width: 180,
-          child: Text(
-            movie.categories.map((category) => category.name).join(", "),
-            style: const TextStyle(
-              fontSize: 12,
-              color: Colors.grey,
+          SizedBox(
+            width: 180,
+            child: Text(
+              movie.categories.map((category) => category.name).join(", "),
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+              overflow: TextOverflow.ellipsis,
             ),
-            overflow: TextOverflow.ellipsis,
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
