1b625bbfd3c36ae542f5de724aea43cb6e92d69c
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-08-05 07:29:01 +0700

    feat: [Cinema Mobile App] Create province provider

---
+++ lib/providers/province_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../services/province_service.dart';
+import '../models/province.dart';
+
+class ProvinceProvider extends ChangeNotifier {
+  List<Province> _provinces = [];
+  List<Province> get provinces => _provinces;
+
+  Province? _selectedProvince;
+  Province? get selectedProvince => _selectedProvince;
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  Future<void> getProvinces() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _provinces = await ProvinceService().getProvinces();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void setSelectedProvince(Province province) {
+    _selectedProvince = province;
+    notifyListeners();
+  }
+}
